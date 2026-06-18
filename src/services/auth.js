4ecd3b835af51107f4a4ac51c9294646fe35ce3d
phevabb <phevab1@gmail.com>
2026-06-18 16:22:32 +0000

    to ngin ex

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
 
 
-   baseURL: 'http://162.35.163.168:9000', // production
+   baseURL: 'https://kogschool.com/business', // production
 
   // baseURL: 'http://127.0.0.1:9000', // local development
   headers: {
